0eb5a73cc21ff79b076caea9abd61fed7603fa82
alina <alina.bregvadze@gmail.com>
2021-10-17 15:35:17 +0400

    deleted unnecessary import

--- src/test/java/Homework_4.java
+++ src/test/java/Homework_4.java
@@ -5,7 +5,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Selectors.byClassName;
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 
